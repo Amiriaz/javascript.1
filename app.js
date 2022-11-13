46989b98f18925cf43de80c5ed646cfcd537e921
Amir Riaz <116000783+Amiriaz@users.noreply.github.com>
2022-11-14 00:33:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,109 @@
+// window.alert ("thanks for your input");
+// var floogle = "Mark"; 
+// console.log(floogle);
+
+//  var lessonAuthor = "Mark";
+//  console.log(lessonAuthor);
+
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+//  var x = "Mark";
+// console.log(x)
+
+// var nationality;
+// nationality = "pakistan";
+// console.log(nationality);
+
+//  var caseQty = 144;
+//  console.log(caseQty);
+
+//  var weight = 150;
+//  weight + 25;
+//  console.log(weight);
+
+//  var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(originalNum + newNum);
+
+// var originalNum = 23;
+//  var numToBeAdded = 7;
+//  var newNum = originalNum + numToBeAdded;
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+// console.log(originalNum = numToBeAdded = originalNum);
+
+// var originalNum = "23";
+// var newNum = originalNum + 7; 
+// console.log(originalNum = newNum);
+
+// var President = "Washington";
+// var prezWhoCame1st = "Washington";
+// console.log(President +  prezWhoCame1st);
+
+// var userResponse;
+// console.log(userResponse);
+// var userResponseTime;
+// console.log(userResponseTime);
+// var userResponseTimeLimit;
+// console.log(userResponseTimeLimit);
+// var response;
+// console.log(response);
+
+
+//  var popularNumber = 4;
+//   var popularNumber = 2 + 2;
+//   console.log(2 + 2);
+
+//   var popularNumber = 12 -24;
+//   var popularNumber = 3 * 12;
+//   console.log(popularNumber = popularNumber);
+
+//   var num = 10;
+//   var popularNumber = num + 200;
+//   console.log(num = popularNumber);
+
+//  var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// console.log(num = anotherNum = popularNumber);
+
+// var whatsLeftOver = 10 % 3;
+// var whatsLeft = 9 % 3;
+// console.log(whatsLeftOver = whatsLeft);   
+   
+// num++;
+// console.log(num);
+
+// num = num + 1;
+// console.log(num = num);
+//  num--;
+//  console.log(num);
+
+// var num = 1;
+// console.log(num);
+
+// var newNum = num++;
+//  var num = 1;
+//  console.log(newNum = num );
+// var newNum = num--;
+// console.log(newNum = num);
+
+
+//  var totalCost = 1 + 3 * 4;
+//  console.log(totalCost);
+//   var totalCost = 1 + (3 * 4);
+//   console.log(totalCost);
+//  var totalCost = (1 + 3) * 4;
+//  console.log(totalCost);
+//   var resultOfComputation = (2 * 4) * 4 + 2;
+//   console.log(resultOfComputation);
+//   resultOfComputation = ((2 * 4) * 4) + 2;
+//   resultOfComputation = (2 * 4) * (4 + 2);
+//   console.log("2 plus 2 equals " + 2 + 2);
+
+ var message = "Thanks, ";
+  var userName = "Susan";
+  var banger = "!";
+  var customMess = message + userName + banger;
+ 
+ console.log(customMess);
+ console.log("2" + "2");
